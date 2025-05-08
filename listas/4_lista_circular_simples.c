aea516205cba0ec66514613144604538a6ae39ef
Francisco Neto <netofrancisco.pro@gmail.com>
2025-05-07 22:25:58 -0300

    chore: alterar nomes de arquivos e preparar arquivos de listas

---
+++ listas/4_lista_circular_simples.c
@@ -0,0 +1,6 @@
+// Uma lista circular simples é uma lista onde o último nó aponta de volta para o primeiro nó,
+// criando um ciclo contínuo. Isso permite que você percorra
+// a lista indefinidamente sem encontrar um NULL no final.
+
+#include <stdio.h>
+#include <stdlib.h>
---
+++ listas/5_lista_circular_duplamente_ligada.c
@@ -0,0 +1,8 @@
+// Uma lista circular duplamente encadeada é uma lista onde:
+// 1. O último nó aponta de volta para o primeiro nó.
+// 2. E o primeiro nó aponta para o último, formando um ciclo completo.
+// Cada nó tem dois ponteiros, um para o próximo nó e outro para o nó anterior, permitindo
+// percorrer a lista tanto para frente quanto para trás de forma circular.
+
+#include <stdio.h>
+#include <stdlib.h>
